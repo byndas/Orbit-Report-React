61fd876fe0e4164516e4df2b8f8f8cc32e5943de
Marc Byndas <byndasma@gmail.com>
2024-03-15 20:56:14 -0400

    active-btn class onclick added in Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,18 +1,28 @@
 import satData from "./satData";
+import {useState} from "react";
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   // makes buttons for each orbit type
+  const [activeIndex, setActiveIndex] = useState(null);
+
   const orbitPathBtns = displaySats.map((sat, index) => {
     return (
-      <button onClick={() => filterByType(sat)} key={index}>
+      <button className={activeIndex === index ? "active-btn" : ""} onClick={() => {
+        filterByType(sat);
+        setActiveIndex(index);
+      }}
+        key={index}>
         {sat} Orbit
       </button>
     );
   })
+
   return (
     <div className="flex-container">
       {orbitPathBtns}
-      <button onClick={() => setSat(satData)}>All Orbits</button>
+      <button className={activeIndex === 3 ? "active-btn" : ""} onClick={() => {
+        setSat(satData)
+        setActiveIndex(3)}}>All Orbits</button>
     </div>
   );
 };
